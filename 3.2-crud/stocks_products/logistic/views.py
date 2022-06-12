2337c80c40486a6d44b87a0485518c85bcdd9ee2
danko1521 <vip.danko1521@mail.ru>
2022-06-12 23:07:17 +0300

    crud 3.2

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -3,14 +3,20 @@ from rest_framework.viewsets import ModelViewSet
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
 
+from rest_framework.viewsets import ViewSet, ModelViewSet
+
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    # при необходимости добавьте параметры фильтрации
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    search_fields = ['title', 'description', ]
+    pagination_class = LimitOffsetPagination
 
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
-    # при необходимости добавьте параметры фильтрации
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['address', 'products', ]
+    pagination_class = LimitOffsetPagination
